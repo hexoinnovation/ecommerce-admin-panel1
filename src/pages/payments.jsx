086553e263f2e5c70e314e0181c09feeb7850ab6
MathumithaG-2004 <hexoinnovation2@gmail.com>
2025-02-04 16:29:21 -0800

    mathu

--- src/pages/payments.jsx
+++ src/pages/payments.jsx
@@ -165,11 +165,11 @@ const PaymentMethods = () => {
           placeholder="Search Payment Methods"
           className="w-full md:w-1/2 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
         />
-        {/* Bulk Enable/Disable */}
+  
        
       </div>
 
-      {/* Grid Layout for Payment Methods */}
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {filteredPaymentMethods.map((method) => (
           <div
@@ -194,8 +194,9 @@ const PaymentMethods = () => {
               <p className="text-sm text-gray-500">{method.description}</p>
             </div>
             <button
+
   onClick={() => {
-    // Show confirmation alert
+  
     const isConfirmed = window.confirm("Are you sure you want to delete this payment method?");
     if (isConfirmed) {
       // Proceed with deletion if user confirms
@@ -274,30 +275,7 @@ const PaymentMethods = () => {
           </div>
         </div>
       )}
-      {/* Confirmation Dialog
-      {isRemoving !== null && (
-        <div className="fixed inset-0 bg-gray-700 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl font-semibold text-gray-700 mb-4">
-              Are you sure you want to remove this payment method?
-            </h2>
-            <div className="flex space-x-4">
-              <button
-                onClick={confirmRemove}
-                className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
-              >
-                Yes, Remove
-              </button>
-              <button
-                onClick={cancelRemove}
-                className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400"
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )} */}
+     
     </div>
   );
 };
